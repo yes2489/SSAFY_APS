b7aeda5471759d010636d8247a8e2f5594a6fa05
EunSeo Yang <yes24150@gmail.com>
2024-08-14 20:32:55 +0900

    SWEA_5658_보물상자

--- day11_0814수/src/SWEA_5658_보물상자/Solution.java
+++ day11_0814수/src/SWEA_5658_보물상자/Solution.java
@@ -5,51 +5,45 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 import java.util.Scanner;
-
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Collections;
 
 public class Solution {
-	static List<String> resA;
+	static List<String> res;
 	static char[] list;
 	static Queue<Character> queue;
-	
-	
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		
+
 		int T = sc.nextInt(); // 총 testcase 수
-		
+
 		queue = new LinkedList<>();
-		
+
 		for (int t = 1; t <= T; t++) {
 			int N = sc.nextInt(); // 입력받을 숫자의 개수
 			int K = sc.nextInt(); // K번째로 큰 수
-			
-			String testnum = sc.next(); // N개의 숫자
-			
-			list = new char[testnum.length()]; 
-			resA = new ArrayList<>();
-			
-			for (int i = 0; i < testnum.length(); i++) {
-				 list[i] = testnum.charAt(i);
+
+			String testNum = sc.next(); // N개의 숫자
+
+			list = new char[testNum.length()]; 
+			res = new ArrayList<>();
+
+			for (int i = 0; i < testNum.length(); i++) {
+				 list[i] = testNum.charAt(i);
 			} // list에 N개 숫자 삽입
-			
-			
+
 			for (int i = 0; i < N/4; i++) {
-				// 회전 할 때 마다 queue 채우기
-				for (int j = i; j < list.length; j++) {
-					queue.offer(list[j]);
-				}
-				// 1회전부터는 앞에서 빠진 수 끝에 추가하기
-				if (i != 0 ) {
-					for (int j = 0; j < i; j++) {
-						queue.offer(list[j]);
-					}
-				}
-				// 다시 보니...마지막에 추가된 수를 뽑고 맨 앞에 추가해야 하네여....ㅎ..ㅎ.ㅎㅎ.ㅎ.ㅎ.ㅎㅎㅎ....
-				
-				
+           		 // i번째 회전 상태에 맞게 숫자를 큐에 채우기
+                for (int j = 0; j < N; j++) {
+               		 // 현재 회전 상태에서의 인덱스를 계산 (시계방향 회전)
+                    int index = (j + i) % N;
+                    queue.offer(list[index]);
+                 }
+
 				String[] arr = new String[4];
-				
+
 				for (int j = 0; j < 4; j++) {
 					String pass = "";
 					for (int k = 0 ; k < N/4; k++) {
@@ -58,29 +52,28 @@ public class Solution {
 					arr[j] = pass;
 				}
 				for(int j = 0; j < 4; j++) {
-					resA.add(arr[j]);
+					res.add(arr[j]);
 				}			
 			}// N/4 회전 끝
-			
-			// 16진수로...만들기...
 
-			
 			// 중복 제거
-
+			Set<String> uniqueNum = new HashSet<>(res);
 			
+			// 16진수로 변환 후 10진수 리스트로 변환
+			List<Integer> sortNum = new ArrayList<>();
+            
+			for (String s : uniqueNum) {
+				sortNum.add(Integer.parseInt(s, 16));
+			}
+
 			// 정렬
+			Collections.sort(sortNum, Collections.reverseOrder());
 
-			
 			// k번째 큰 수
-			
-			
-			// 출력
-//			for (int i = 0; i < resA.size(); i++) {
-//				System.out.print(resA.get(i) + " ");
-//			}
-//			확인용....하핫
+			int result = sortNum.get(K-1);
 
+			// 출력
+			System.out.println("#" + t + " " + result);
 		}
 	}
-
 }
